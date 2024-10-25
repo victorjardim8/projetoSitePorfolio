e98ed334bb6c81ef38055f993221812ea9b76280
Victor Jardim <victor.jardim@gomara.tech>
2024-10-25 15:12:29 -0300

    correcoes

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+document.querySelector('#instagram').addEventListener("click", function(){
+    window.open("https://www.instagram.com/amandaapoxa/", "_blank");
+});
+
+document.querySelector('#tiktok').addEventListener("click", function(){
+    window.open("https://www.instagram.com/amandaapoxa/", "_blank");
+});
+
+document.querySelector('#youtube').addEventListener("click", function(){
+    window.open("https://www.tiktok.com/@amandaapoxa", "_blank")
+});
